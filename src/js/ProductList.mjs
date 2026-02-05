93145908c32260b27dd19899a0ec3487429bf3f6
Jared Samayoa <jsamayoapenagos@byupathway.edu>
2026-02-04 22:45:57 -0600

    W03 Individual Activity: Expand Inventory

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -8,18 +8,20 @@ export default class ProudctList {
     }
     
     async init(){
-        const list = await this.dataSource.getData();
+        const list = await this.dataSource.getData(this.category);
         this.renderList(list);
     }
 
     productCardTemplate(product){
         return (
           `<li class="product-card">
-            <a href="product_pages/?product=${product.Id}">
-              <img
-                src="${product.Image}"
-                alt="${product.Name}"
-              />
+            <a href="/product_pages/?product=${product.Id}">
+              <div class="product-card-image">
+                <img
+                  src="${product.Images.PrimaryMedium}"
+                  alt="${product.Name}"
+                />
+              </div>
               <h3 class="card__brand">${product.Brand.Name}</h3>
               <h2 class="card__name">${product.Name}</h2>
               <p class="product-card__price">$${product.FinalPrice}</p>
