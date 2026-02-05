74895f54de6055841a720a12afe1290b3e5b575d
Jared Samayoa <jsamayoapenagos@byupathway.edu>
2026-02-04 20:41:19 -0600

    W03 Team Activity: Dynamic Header and Footer (Stretch Challenge)

---
+++ src/js/ShoppingCart.mjs
@@ -0,0 +1,40 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+export default class ShoppingCart {
+    constructor (dataSource, listElement){
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+    
+    async init(){
+        this.renderList(this.dataSource);
+    }
+
+    cartItemTemplate(item) {
+        return (
+            `<li class="cart-card divider">
+                <a href="#" class="cart-card__image">
+                    <img
+                    src="${item.Image}"
+                    alt="${item.Name}"
+                    />
+                </a>
+                <a href="#">
+                    <h2 class="card__name">${item.Name}</h2>
+                </a>
+                <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+                <p class="cart-card__quantity">qty: 1</p>
+                <p class="cart-card__price">$${item.FinalPrice}</p>
+            </li>`
+        );
+    }
+
+    renderList(products){
+        // const formattedProducts = products.map(this.productCardTemplate).join("");
+        
+        // //this.listElement.insertAdjacentHTML("beforebegin", formattedProducts);
+        // this.listElement.innerHTML = formattedProducts;
+
+        renderListWithTemplate(this.cartItemTemplate, this.listElement, products);
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,6 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { setCartItemsNumber, loadHeaderFooter } from "./utils.mjs";
 
-// loadHeaderFooter();
-// const dataSource = new ProductData("tents");
-// const listElement = document.querySelector(".product-list");
-// const productList = new ProductList(
-//   dataSource.category,
-//   dataSource,
-//   listElement,
-// );
-// productList.init();
-// setCartItemsNumber();
-
-
 async function init() {
   await loadHeaderFooter();
 
